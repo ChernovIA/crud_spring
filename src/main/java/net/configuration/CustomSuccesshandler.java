940902a35454001dca60f438176db1e1ddfd6b0e
ChernovIA <Chernov.ilya.alekseevich@gmail.com>
2018-09-14 18:26:22 +0300

    fix

--- src/main/java/net/configuration/CustomSuccesshandler.java
+++ src/main/java/net/configuration/CustomSuccesshandler.java
@@ -1,13 +1,9 @@
 package net.configuration;
 
 import net.model.Role;
-import net.model.RolesTypes;
-import net.model.User;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@Component("CustomSuccesshandler")
+@Service("CustomSuccesshandler")
 public class CustomSuccesshandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
